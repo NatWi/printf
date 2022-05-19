269c39f25406c8a1c9860a88c0017e2514207b7f
Praktikumspartner <drumdikow2@gmx.de>
2022-05-19 08:21:02 +0200

    Data types

---
+++ format.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main(){
+	int var_int_number=67;
+	float var_float_number=99.43;
+	char var_character='z';
+	char var_string[]="Die Zeit ist im Umbruch!";
+
+	printf("Ausgabe Ganzzahl: %i \n", var_int_number);
+	printf("Ausgabe Gleitkommazahl: %f \n", var_float_number);
+	printf("Ausgabe Character: %c \n", var_character);
+	printf("Ausgabe String: %s \n", var_string);
+	return 0;
+}
